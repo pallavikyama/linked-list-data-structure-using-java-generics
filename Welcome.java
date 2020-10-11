a65ef0eb1041d4cfff2f7027caf3110e096ad49c
pallavikyama <kyamap98@gmail.com>
2020-10-11 19:35:09 +0530

    Added:Welcome to Linked-ist data structure using Java generics message is added.

---
+++ Welcome.java
@@ -0,0 +1,7 @@
+package com.blz.datastructures;
+
+public class Welcome {
+	public static void main(String[] args) {
+		System.out.println("Welcome to creating Linked-List data structure using Java generics.");
+	}
+}
