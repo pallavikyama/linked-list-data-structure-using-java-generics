e779ed48ae85f692e7a6d9072c1f04bdbeadf9ec
pallavikyama <kyamap98@gmail.com>
2020-10-12 08:20:34 +0530

    Refactored:All testcases are considered

--- LinkedListTest.java
+++ LinkedListTest.java
@@ -1,27 +1,191 @@
 package com.blz.datastructures;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 public class LinkedListTest {
 
 	@Test
-	public void TestUC10() {
-		SortedLinkedList<Integer> myList = new SortedLinkedList<Integer>();
+	public void TestUC01() {
+		System.out.println("\nUC-01:");
+		MyLinkedList<Integer> myList = new MyLinkedList<>();
 		MyNode<Integer> firstNode = new MyNode<Integer>(56);
+		MyNode<Integer> secondNode = new MyNode<Integer>(30);
+		MyNode<Integer> thirdNode = new MyNode<Integer>(70);
+		myList.head = firstNode;
+		firstNode.setNext(secondNode);
+		secondNode.setNext(thirdNode);
+		myList.tail = thirdNode;
+		System.out.println("Created a simple Linked-list.");
+		myList.printNodes();
+		assertEquals(firstNode.getNext(), secondNode);
+		assertEquals(secondNode.getNext(), thirdNode);
+	}
+
+	@Test
+	public void TestUC02() {
+		System.out.println("\nUC-02:");
+		MyLinkedList<Integer> myList = new MyLinkedList<>();
+		MyNode<Integer> firstNode = new MyNode<Integer>(70);
 		myList.add(firstNode);
 		MyNode<Integer> secondNode = new MyNode<Integer>(30);
 		myList.add(secondNode);
-		MyNode<Integer> thirdNode = new MyNode<Integer>(40);
+		MyNode<Integer> thirdNode = new MyNode<Integer>(56);
 		myList.add(thirdNode);
-		MyNode<Integer> fourthNode = new MyNode<Integer>(70);
-		myList.add(fourthNode);
+		System.out.println("Created a simple Linked-list using add method.");
 		myList.printNodes();
-		assertEquals(myList.head, secondNode);
-		assertEquals(myList.tail, fourthNode);
+		assertEquals(myList.head, thirdNode);
+		assertEquals(myList.head.getNext(), secondNode);
+		assertEquals(myList.tail, firstNode);
+	}
+
+	@Test
+	public void TestUC03() {
+		System.out.println("\nUC-03:");
+		MyLinkedList<Integer> myList = new MyLinkedList<>();
+		MyNode<Integer> firstNode = new MyNode<Integer>(56);
+		myList.add(firstNode);
+		MyNode<Integer> secondNode = new MyNode<Integer>(30);
+		myList.append(secondNode);
+		MyNode<Integer> thirdNode = new MyNode<Integer>(70);
+		myList.append(thirdNode);
+		System.out.println("Created a simple Linked-list using append method.");
+		myList.printNodes();
+		assertEquals(myList.head, firstNode);
+		assertEquals(myList.head.getNext(), secondNode);
+		assertEquals(myList.tail, thirdNode);
+	}
+
+	@Test
+	public void TestUC04() {
+		System.out.println("\nUC-04:");
+		MyLinkedList<Integer> myList = new MyLinkedList<>();
+		MyNode<Integer> firstNode = new MyNode<Integer>(56);
+		myList.add(firstNode);
+		MyNode<Integer> secondNode = new MyNode<Integer>(70);
+		myList.append(secondNode);
+		MyNode<Integer> thirdNode = new MyNode<Integer>(30);
+		myList.insert(firstNode, thirdNode);
+		System.out.println("Created a simple Linked-list using insert method.");
+		myList.printNodes();
+		assertEquals(myList.head, firstNode);
+		assertEquals(myList.tail, secondNode);
 		assertEquals(myList.head.getNext(), thirdNode);
+	}
+
+	@Test
+	public void TestUC05() {
+		System.out.println("\nUC-05:");
+		MyLinkedList<Integer> myList = new MyLinkedList<>();
+		MyNode<Integer> firstNode = new MyNode<Integer>(56);
+		myList.add(firstNode);
+		MyNode<Integer> secondNode = new MyNode<Integer>(70);
+		myList.append(secondNode);
+		MyNode<Integer> thirdNode = new MyNode<Integer>(30);
+		myList.insert(firstNode, thirdNode);
+		System.out.println("Removing " + myList.pop().getData() + ".");
+		myList.printNodes();
+		assertEquals(myList.head, thirdNode);
+		assertEquals(myList.tail, secondNode);
+		assertEquals(myList.head.getNext(), secondNode);
+	}
+
+	@Test
+	public void TestUC06() {
+		System.out.println("\nUC-06:");
+		MyLinkedList<Integer> myList = new MyLinkedList<>();
+		MyNode<Integer> firstNode = new MyNode<Integer>(56);
+		myList.add(firstNode);
+		MyNode<Integer> secondNode = new MyNode<Integer>(70);
+		myList.append(secondNode);
+		MyNode<Integer> thirdNode = new MyNode<Integer>(30);
+		myList.insert(firstNode, thirdNode);
+		System.out.println("Removing " + myList.popLast().getData() + ".");
+		myList.printNodes();
+		assertEquals(myList.head, firstNode);
+		assertEquals(myList.tail, thirdNode);
+		assertEquals(myList.head.getNext(), thirdNode);
+	}
+
+	@Test
+	public void TestUC07() {
+		System.out.println("\nUC-07:");
+		MyLinkedList<Integer> myList = new MyLinkedList<>();
+		MyNode<Integer> firstNode = new MyNode<Integer>(56);
+		myList.add(firstNode);
+		MyNode<Integer> secondNode = new MyNode<Integer>(70);
+		myList.append(secondNode);
+		MyNode<Integer> thirdNode = new MyNode<Integer>(30);
+		myList.insert(firstNode, thirdNode);
+		System.out.println("Found the data. Search successful!");
+		myList.printNodes();
+		assertEquals(myList.head, firstNode);
+		assertEquals(myList.tail, secondNode);
+		assertEquals(myList.head.getNext(), thirdNode);
+		assertTrue(myList.search(30).getData().equals(30));
+	}
+
+	@Test
+	public void TestUC08() {
+		System.out.println("\nUC-08:");
+		MyLinkedList<Integer> myList = new MyLinkedList<>();
+		MyNode<Integer> firstNode = new MyNode<Integer>(56);
+		myList.add(firstNode);
+		MyNode<Integer> secondNode = new MyNode<Integer>(70);
+		myList.append(secondNode);
+		MyNode<Integer> thirdNode = new MyNode<Integer>(30);
+		myList.insert(firstNode, thirdNode);
+		MyNode<Integer> fourthNode = new MyNode<Integer>(40);
+		myList.insert(myList.search(30), fourthNode);
+		System.out.println("Added a new node into the Linked-list using insert and search methods.");
+		myList.printNodes();
+		assertEquals(myList.head, firstNode);
+		assertEquals(myList.tail, secondNode);
+		assertEquals(myList.search(30).getNext(), fourthNode);
+	}
+
+	@Test
+	public void TestUC09() {
+		System.out.println("\nUC-09:");
+		MyLinkedList<Integer> myList = new MyLinkedList<>();
+		MyNode<Integer> firstNode = new MyNode<Integer>(56);
+		myList.add(firstNode);
+		MyNode<Integer> secondNode = new MyNode<Integer>(70);
+		myList.append(secondNode);
+		MyNode<Integer> thirdNode = new MyNode<Integer>(30);
+		myList.insert(firstNode, thirdNode);
+		MyNode<Integer> fourthNode = new MyNode<Integer>(40);
+		myList.insert(myList.search(30), fourthNode);
+		myList.delete(40);
+		System.out.println("Removing " + fourthNode.getData() + ".");
+		myList.printNodes();
+		assertEquals(myList.head, firstNode);
+		assertEquals(myList.tail, secondNode);
+		assertEquals(myList.head.getNext(), thirdNode);
+		assertEquals(thirdNode.getNext(), myList.tail);
+		assertEquals(3, myList.size());
+	}
+
+	@Test
+	public void TestUC10() {
+		System.out.println("\nUC-10:");
+		SortedLinkedList<Integer> mySortedList = new SortedLinkedList<Integer>();
+		MyNode<Integer> firstNode = new MyNode<Integer>(56);
+		mySortedList.add(firstNode);
+		MyNode<Integer> secondNode = new MyNode<Integer>(30);
+		mySortedList.add(secondNode);
+		MyNode<Integer> thirdNode = new MyNode<Integer>(40);
+		mySortedList.add(thirdNode);
+		MyNode<Integer> fourthNode = new MyNode<Integer>(70);
+		mySortedList.add(fourthNode);
+		System.out.println("Created a sorted/ordered Linked-list using insert method.");
+		mySortedList.printNodes();
+		assertEquals(mySortedList.head, secondNode);
+		assertEquals(mySortedList.tail, fourthNode);
+		assertEquals(mySortedList.head.getNext(), thirdNode);
 		assertEquals(thirdNode.getNext(), firstNode);
-		assertEquals(firstNode.getNext(), myList.tail);
+		assertEquals(firstNode.getNext(), mySortedList.tail);
 	}
 }
---
+++ MyLinkedList.java
@@ -0,0 +1,117 @@
+package com.blz.datastructures;
+
+public class MyLinkedList<E> {
+	NodeInterface<E> head;
+	NodeInterface<E> tail;
+
+	public MyLinkedList() {
+		this.head = null;
+		this.tail = null;
+	}
+
+	// ADDING NEW NODE IN THE FRONT IN LIST
+	public void add(NodeInterface<E> newNode) {
+		if (this.head == null)
+			this.head = newNode;
+		if (this.tail == null)
+			this.tail = newNode;
+		else {
+			NodeInterface<E> tempNode = this.head;
+			this.head = newNode;
+			this.head.setNext(tempNode);
+		}
+	}
+
+	// APPENDING NEW NODE IN THE LAST IN LIST
+	public void append(NodeInterface<E> newNode) {
+		if (this.head == null)
+			this.head = newNode;
+		if (this.tail == null)
+			this.tail = newNode;
+		else {
+			this.tail.setNext(newNode);
+			this.tail = newNode;
+		}
+	}
+
+	// INSERTING NEW NODE AFTER A SPECIFIC NODE
+	public void insert(NodeInterface<E> refNode, NodeInterface<E> newNode) {
+		newNode.setNext(refNode.getNext());
+		refNode.setNext(newNode);
+	}
+
+	// DELETING FIRST ELEMENT IN THIS LIST
+	public NodeInterface<E> pop() {
+		NodeInterface<E> tempNode = this.head;
+		this.head = this.head.getNext();
+		return tempNode;
+	}
+
+	// DELETING LAST ELEMENT IN THIS LIST
+	public NodeInterface<E> popLast() {
+		NodeInterface<E> tempNode = this.head;
+		while (!tempNode.getNext().equals(this.tail))
+			tempNode = tempNode.getNext();
+		this.tail = tempNode;
+		tempNode = tempNode.getNext();
+		this.tail.setNext(null);
+		return tempNode;
+	}
+
+	// SEARCHING FOR A NODE USING ITS DATA VALUE
+	public NodeInterface<E> search(E data) {
+		NodeInterface<E> tempNode = this.head;
+		while (tempNode.getNext() != null && tempNode != null) {
+			if (tempNode.getData().equals(data))
+				return tempNode;
+			else
+				tempNode = tempNode.getNext();
+		}
+		return null;
+	}
+
+	// DELETING SPECIFIED NODE USING SEARCH-METHOD ABOVE
+	public NodeInterface<E> delete(E data) {
+		NodeInterface<E> tempNode = this.head;
+		if (search(data) == null)
+			return search(data);
+		if (search(data).equals(this.head))
+			return pop();
+		if (search(data).equals(this.tail))
+			return popLast();
+		else {
+			while (!tempNode.getNext().equals(search(data))) {
+				tempNode = tempNode.getNext();
+			}
+			tempNode.setNext(search(data).getNext());
+		}
+		return search(data);
+	}
+
+	// GET CURRENT SIZE OF THIS LIST
+	public int size() {
+		NodeInterface<E> tempNode = this.head;
+		int value = 0;
+		if (tempNode != null)
+			while (!tempNode.equals(this.tail)) {
+				++value;
+				tempNode = tempNode.getNext();
+			}
+		else
+			return 0;
+		return ++value;
+	}
+
+	public void printNodes() {
+		StringBuffer allNodes = new StringBuffer();
+		NodeInterface<E> tempNode = this.head;
+		while (tempNode.getNext() != null) {
+			allNodes.append(tempNode.getData());
+			if (!tempNode.equals(tail))
+				allNodes.append("->");
+			tempNode = tempNode.getNext();
+		}
+		allNodes.append(tempNode.getData());
+		System.out.println("Displaying all my nodes:  " + allNodes);
+	}
+}
